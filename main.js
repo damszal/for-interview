ca1cee0251187f98d8ac51d65237a2ac0c4c0f26
damian <damian.szalimow@onet.eu>
2022-07-25 00:11:24 +0200

    test

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
 
 let cl = console.log
 
-
-let imgOut = document.querySelector('.new-slider')
-
-let count = 0
-let index = 0
-
-//cl(imgOut.children[count].style.zIndex = count)
-
-const slide = function() { setInterval(() => {
-    (count>2)?count=0:count;
-    (index>2)?index=0:index;
-    imgOut.children[0].style.zIndex = 0
-    imgOut.children[1].style.zIndex = 0
-    imgOut.children[2].style.zIndex = 0
-    index++
-    imgOut.children[count].style.zIndex = index
-    count++
-}, 2000);}
-
-slide()
-
-console.log("skrypt działa")
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-const v = document.querySelector('.search-bar-input-area  input')
-
-console.log(v)
+cl("plik ze skryptem działa")
